293b22e27b9ec8c38bdcf9d11b1ca97e29e8e448
JorgeFilipi <160806240+JorgeFilipi@users.noreply.github.com>
2024-10-26 14:26:36 -0300

    Aula 02

--- gestao_alugueis/urls.py
+++ gestao_alugueis/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-#from xml.etree.ElementInclude import include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
--- imoveis/urls.py
+++ imoveis/urls.py
@@ -2,12 +2,20 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+
     path('', views.index, name='index'),
     path('login/', views.user_login, name='login'),
     path('logout/', views.user_logout, name='logout'),
     path('imoveis/', views.list_imoveis, name='list_imoveis'),
 
+
+    path('imoveis/adicionar/', views.adicionar_imovel, name='adicionar_imovel'),
     path('imoveis/editar/<int:imovel_id>', views.editar_imovel, name='editar_imovel'),
     path('imoveis/excluir/<int:imovel_id>', views.excluir_imovel, name='excluir_imovel'),
+
+    path('relatorios/relatorio_pagamentos/', views.relatorio_pagamentos, name='relatorio_pagamentos'),
+
     path('inquilinos/', views.list_inquilinos, name='list_inquilinos'),
+    path('inquilinos/adicionar/', views.adicionar_inquilino, name='adicionar_inquilino'),
+    path('inquilinos/editar/<int:inquilino_id>', views.editar_inquilino, name='editar_inquilino'),
 ]
